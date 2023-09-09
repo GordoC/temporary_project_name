c987ec6d8d06d6c2ccda03a0187081bbca94bb52
Erika <erika_cats@Erikas-MacBook-Pro.local>
2023-09-09 11:01:09 -0700

    Update indent

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,29 +4,29 @@ import NavigationPage from './pages/NavigationPage';
 import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
-	return (
-		// <div className="App">
-		//   <header className="App-header">
-		//     <img src={logo} className="App-logo" alt="logo" />
-		//     <p>
-		//       Edit <code>src/App.tsx</code> and save to reload.
-		//     </p>
-		//     <a
-		//       className="App-link"
-		//       href="https://reactjs.org"
-		//       target="_blank"
-		//       rel="noopener noreferrer"
-		//     >
-		//       Learn React
-		//     </a>
-		//   </header>
-		// </div>
-		<div>
-			{/* <HomePage /> */}
-			<NavigationPage />
-			{/* <RegistrationPage /> */}
-		</div>
-	)
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      {/* <HomePage /> */}
+      {/* <NavigationPage /> */}
+      <RegistrationPage />
+    </div>
+  )
 }
 
 export default App;
